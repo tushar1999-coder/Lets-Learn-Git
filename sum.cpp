e07426585f8eb2421bdf904643f8dc604e91ff55
Tushar <tusharvishi2006@gmail.com>
2022-01-22 16:12:58 +0530

    intial commit

---
+++ sum.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define f(i, n) for (int i = 0; i < n; i++)
+#define fa(i, a, n) for (int i = a; i < n; i++)
+#define fit(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define fc(current, a) for(auto current:a)
+
+#define endl "\n"
+#define M 1000000007
+#define PI 3.1415926535897932384626
+
+#define print(x) cout << #x << "=" << x << "\n"
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+
+#define clr(x) memset(x,0, sizeof(x))
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+
+
+void solve(){
+     
+} 
+
+int main() 
+{ 
+  
+    int a=3;
+    int b=10;
+    int sum=a+b;
+    cout << sum; 
+    
+    //cout << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl; 
+    return 0; 
+} 
