17c96e94d2d199b28cb6f5f076b4765c42c14b84
Tushar <tusharvishi2006@gmail.com>
2022-01-22 18:08:03 +0530

    added message in sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -3,8 +3,8 @@ using namespace std;
 #define ll long long
 #define f(i, n) for (int i = 0; i < n; i++)
 #define fa(i, a, n) for (int i = a; i < n; i++)
-#define fit(it, a) for(auto it = a.begin(); it != a.end(); it++)
-#define fc(current, a) for(auto current:a)
+#define fit(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define fc(current, a) for (auto current : a)
 
 #define endl "\n"
 #define M 1000000007
@@ -19,7 +19,7 @@ using namespace std;
 #define all(x) x.begin(), x.end()
 #define sortall(x) sort(all(x))
 
-#define clr(x) memset(x,0, sizeof(x))
+#define clr(x) memset(x, 0, sizeof(x))
 
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
@@ -27,19 +27,18 @@ typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef vector<pii> vpii;
 
+void solve()
+{
+}
 
-void solve(){
-     
-} 
+int main()
+{
 
-int main() 
-{ 
-  
-    int a=3;
-    int b=10;
-    int sum=a+b;
-    cout << sum; 
-    
-    //cout << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl; 
-    return 0; 
-} 
+    int a = 3;
+    int b = 10;
+    int sum = a + b;
+    cout << "the sum is " << sum;
+
+    //cout << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
